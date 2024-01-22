85a429c64454f16a9fb79edb8ebf1c93e80935a1
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-01-22 18:48:30 +0300

    :art: Modify registration UI

--- front/src/components/Register.js
+++ front/src/components/Register.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Register() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const BASE_URL = 'http://localhost:8000/api/register'
+
+  const signUp = async() => {
+    let cred = {name, email, password}
+    console.log(cred)
+
+    let result = await fetch(BASE_URL, {
+      method: 'POST',
+      body: JSON.stringify(cred),
+      headers: {
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    result = await result.json()
+    localStorage.setItem('user-info', JSON.stringify(result))
+    navigate('/add')
+  }
+
   return (
-    <div>Register</div>
+    <div className='col-sm-6 offset-sm-3'>
+      <h1>Registration Page</h1>
+      <input 
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)} 
+        className='form-control' 
+        placeholder='Name' />
+      <br />
+      <input 
+        type='email'
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} 
+        className='form-control' 
+        placeholder='Email' />
+      <br />
+      <input 
+        type='password' 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} 
+        className='form-control' 
+        placeholder='Password' />
+      <br />
+      <button className='btn btn-primary' onClick={signUp}>Sign Up</button>
+    </div>
   )
 }
 
