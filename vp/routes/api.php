86fa3f1bab89c96989043a21ac9fb2f3b5dca595
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-01-22 18:47:52 +0300

    :arrow_up: Update registration api

--- vp/routes/api.php
+++ vp/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\UserController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
