1d116d096365cac6ad6280f33089d6a6aa0a6453
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-01-23 13:01:49 +0300

    :hammer: Create products table and api

---
+++ vp/app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function addProduct(Request $req)
+    {
+        $product = new Product;
+        $product->name=$req->input('name');
+        $product->price=$req->input('price');
+        $product->description=$req->input('description');
+        $product->file_path=$req->file('file')->store('products');
+        $product->save();
+
+        return $product;
+    }
+}
